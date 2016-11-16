3488ee0456fb913b51e0250c3e58d1a0daae0326
Joi Hdez <joel@warptech.mx>
2016-11-15 21:37:29 -0600

    mapa2

--- mapa/mapaSeccion2013.php
+++ mapa/mapaSeccion2013.php
@@ -96,286 +96,287 @@ $titulo .=  ($_GET['redist']==0 ? "2012" : "2015");
 		var datos = <?php echo $datos; ?>;
 		function initialize() {
 
-		//-----------------------------------------------------------------------------------------------
-		<?php
-		//if($lim==1) {
-		//***********************************************************************************************
-		// DISTRITO
-		//***********************************************************************************************
-		/*$dsql9= "SELECT distrito FROM ".$tbl_distrito." GROUP BY distrito";
-		$drs9 = $db->execute($dsql9);
-		while(!$drs9->EOF)
+//-----------------------------------------------------------------------------------------------
+<?php
+//if($lim==1) {
+//***********************************************************************************************
+// DISTRITO
+//***********************************************************************************************
+/*$dsql9= "SELECT distrito FROM ".$tbl_distrito." GROUP BY distrito";
+$drs9 = $db->execute($dsql9);
+while(!$drs9->EOF)
+{
+	$ddistrito = $drs9->fields[0];
+	$dsql1= "SELECT * FROM ".$tbl_distrito." WHERE distrito = '".$ddistrito."' ORDER BY orden";
+	$drs = $db->execute($dsql1);
+	$dsql2= "SELECT distrito, distriton, COUNT(distrito) as num FROM ".$tbl_distrito." WHERE distrito = '".$ddistrito."' GROUP BY distrito ";
+	$drs2 = $db->execute($dsql2);
+	$dnumPuntos = 1;
+	$dultimo = $drs2->fields[2]-1;  //Candidad de puntos por poligono
+	$dfill="#FFF";
+?>
+//COORDENADAS
+  var dCoords0<?php echo $dnumPoly; ?> = [
+<?php
+		while(!$drs->EOF)
 		{
-			$ddistrito = $drs9->fields[0];
-			$dsql1= "SELECT * FROM ".$tbl_distrito." WHERE distrito = '".$ddistrito."' ORDER BY orden";
-			$drs = $db->execute($dsql1);
-			$dsql2= "SELECT distrito, distriton, COUNT(distrito) as num FROM ".$tbl_distrito." WHERE distrito = '".$ddistrito."' GROUP BY distrito ";
-			$drs2 = $db->execute($dsql2);
-			$dnumPuntos = 1;
-			$dultimo = $drs2->fields[2]-1;  //Candidad de puntos por poligono
-			$dfill="#FFF";
-		?>
-		//COORDENADAS
-		  var dCoords0<?php echo $dnumPoly; ?> = [
-		<?php
-				while(!$drs->EOF)
-				{
-					$dcadena= "new google.maps.LatLng(".$drs->fields[4].")";
-					if($dultimo < $dnumPuntos) { $dcadena.="\n"; } else { $dcadena.=",\n"; }
-					echo $dcadena;
-					$dnumPuntos++;
-					$drs->MoveNext();
-				} //while
-		?>
-		  ];
-			var dPol0<?php echo $dnumPoly; ?>;
-		  	dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
-
-		<?php
-			$dnumPoly++;
-			$drs9->MoveNext();
+			$dcadena= "new google.maps.LatLng(".$drs->fields[4].")";
+			if($dultimo < $dnumPuntos) { $dcadena.="\n"; } else { $dcadena.=",\n"; }
+			echo $dcadena;
+			$dnumPuntos++;
+			$drs->MoveNext();
 		} //while
-		} //if limites distritales
-		*/
+?>
+  ];
+	var dPol0<?php echo $dnumPoly; ?>;
+  	dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
 
-		$dsql1= "SELECT * FROM ".$tbl_distrito." ORDER BY distrito, orden";
-		$drs = $db->execute($dsql1);
-		$ini_distrito=$drs->fields[2];
-		$xdistrito[$dnumPoly]=$ini_distrito;
-		$dnumPuntos = 1;
-		?>//COORDENADAS // comentado por Joel
-			/*var dCoords0<?php echo $dnumPoly; ?> = [
-		<?php
-			while(!$drs->EOF)
-			{
-				$fin_distrito = $drs->fields[2];
-				if($ini_distrito==$fin_distrito){
-					$dcadena = "new google.maps.LatLng(".$drs->fields[4].")";
-					if($dnumPuntos==0) { $dcadena.="\n"; } else { $dcadena.=",\n"; }
-					echo $dcadena;
-					$dnumPuntos++;
-				}else{
-		?>
-				];*/ // fin del comentario Joel
-				var dPol0<?php echo $dnumPoly; ?>;
-		  		dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
-		<?php /****reinicilizo porque cambia el distrito************/
-					$xdistrito[$dnumPoly]=$ini_distrito;
-					$dnumPoly++;
-					$dnumPuntos = 1;
-					$ini_distrito = $drs->fields[2];
-		?>
-					var dCoords0<?php echo $dnumPoly; ?> = [
-		<?php
-					$dcadena= "new google.maps.LatLng(".$drs->fields[4].")";
-					$dcadena.=",\n";
-					echo $dcadena;
-					$dnumPuntos++;
-				}
-				$drs->MoveNext();
-			} //while
+<?php
+	$dnumPoly++;
+	$drs9->MoveNext();
+} //while
+} //if limites distritales
+*/
 
+$dsql1= "SELECT * FROM ".$tbl_distrito." ORDER BY distrito, orden";
+$drs = $db->execute($dsql1);
+$ini_distrito=$drs->fields[2];
+$xdistrito[$dnumPoly]=$ini_distrito;
+$dnumPuntos = 1;
+?>//COORDENADAS
+	var dCoords0<?php echo $dnumPoly; ?> = [
+<?php
+	while(!$drs->EOF)
+	{
+		$fin_distrito = $drs->fields[2];
+		if($ini_distrito==$fin_distrito){
+			$dcadena = "new google.maps.LatLng(".$drs->fields[4].")";
+			if($dnumPuntos==0) { $dcadena.="\n"; } else { $dcadena.=",\n"; }
+			echo $dcadena;
+			$dnumPuntos++;
+		}else{
+?>
+		];
+		var dPol0<?php echo $dnumPoly; ?>;
+  		dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
+<?php /****reinicilizo porque cambia el distrito************/
 			$xdistrito[$dnumPoly]=$ini_distrito;
-			?>
-			];
-			var dPol0<?php echo $dnumPoly; ?>;
-		  		dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
-			<?php
 			$dnumPoly++;
+			$dnumPuntos = 1;
+			$ini_distrito = $drs->fields[2];
+?>
+			var dCoords0<?php echo $dnumPoly; ?> = [
+<?php
+			$dcadena= "new google.maps.LatLng(".$drs->fields[4].")";
+			$dcadena.=",\n";
+			echo $dcadena;
+			$dnumPuntos++;
+		}
+		$drs->MoveNext();
+	} //while
+
+	$xdistrito[$dnumPoly]=$ini_distrito;
+	?>
+	];
+	var dPol0<?php echo $dnumPoly; ?>;
+  		dPol0<?php echo $dnumPoly; ?> = new google.maps.Polyline({ path: dCoords0<?php echo $dnumPoly; ?>, strokeColor: '#000', strokeWeight: 1, zIndex:2 });
+	<?php
+	$dnumPoly++;
 
 
-		//***********************************************************************************************
-		// SECCIONES
-		//***********************************************************************************************
+//***********************************************************************************************
+// SECCIONES
+//***********************************************************************************************
 
-			//Consulta: Buscar todas las secciones a desplegar en el mapa por DELEGACION
-			/*$sql9= "SELECT seccion FROM ".$Tabla." GROUP BY seccion";
-			$rs9 = $db->execute($sql9);
-			while(!$rs9->EOF)
+	//Consulta: Buscar todas las secciones a desplegar en el mapa por DELEGACION
+	/*$sql9= "SELECT seccion FROM ".$Tabla." GROUP BY seccion";
+	$rs9 = $db->execute($sql9);
+	while(!$rs9->EOF)
+	{
+		$seccion = $rs9->fields[0];
+		//Consulta: Buscar las coordenadas de la seccion para formar el poligo.
+		$sql1= "SELECT * FROM ".$tbl_seccion." WHERE seccion in (".$seccion.") ORDER BY orden";
+		$rs = $db->execute($sql1);
+		$ndistrito=$rs->fields[1];
+
+		$dsql3="SELECT COUNT(*) FROM redistritacion where tipo=1 and seccion in (".$seccion.")";
+		$drs3 = $db->execute($dsql3);
+		if($drs3->fields[0]==0){
+			$color="#FFFF00";
+			$lcolor="#666";
+		}else{
+			$color="#FF0000";
+			$lcolor="#FCD209";
+		}
+
+		//Optiene datos de la seccion
+		$sql2= "SELECT distrito, seccion, COUNT(seccion) as num FROM ".$tbl_seccion." WHERE seccion in (".$seccion.") GROUP BY seccion";
+		$rs2 = $db->execute($sql2);
+		$numPuntos = 1;
+		$ultimo = $rs2->fields[2]-1;  //Candidad de puntos por poligono
+?>
+		//COORDENADAS
+		var Coords0<?php echo $numPoly; ?> = [
+<?php
+			while(!$rs->EOF)
 			{
-				$seccion = $rs9->fields[0];
-				//Consulta: Buscar las coordenadas de la seccion para formar el poligo.
-				$sql1= "SELECT * FROM ".$tbl_seccion." WHERE seccion in (".$seccion.") ORDER BY orden";
-				$rs = $db->execute($sql1);
-				$ndistrito=$rs->fields[1];
+				$cadena= "new google.maps.LatLng(".$rs->fields[4].")";
+				if($ultimo < $numPuntos) { $cadena.="\n"; } else { $cadena.=",\n"; }
+				echo $cadena;
+				$numPuntos++;
+				$rs->MoveNext();
+			} //while
+?>
+		]; //Coordsx
+		//Crea la variable del numero del poligo
+		var Pol0<?php echo $numPoly; ?>;
+  		//Crea el poligo y las propiedades
+		Pol0<?php echo $numPoly; ?> = new google.maps.Polygon({ paths: Coords0<?php echo $numPoly; ?>, strokeColor: '<?php echo $lcolor; ?>', strokeWeight: 0.8, fillColor: '<?php echo $color; ?>', fillOpacity: '<?php echo $fillop; ?>', zIndex: 1 });
 
-				$dsql3="SELECT COUNT(*) FROM redistritacion where tipo=1 and seccion in (".$seccion.")";
-				$drs3 = $db->execute($dsql3);
-				if($drs3->fields[0]==0){
-					$color="#FFFF00";
-					$lcolor="#666";
-				}else{
-					$color="#FF0000";
-					$lcolor="#FCD209";
-				}
-
-				//Optiene datos de la seccion
-				$sql2= "SELECT distrito, seccion, COUNT(seccion) as num FROM ".$tbl_seccion." WHERE seccion in (".$seccion.") GROUP BY seccion";
-				$rs2 = $db->execute($sql2);
-				$numPuntos = 1;
-				$ultimo = $rs2->fields[2]-1;  //Candidad de puntos por poligono
-		?>
-				//COORDENADAS
-				var Coords0<?php echo $numPoly; ?> = [
-		<?php
-					while(!$rs->EOF)
-					{
-						$cadena= "new google.maps.LatLng(".$rs->fields[4].")";
-						if($ultimo < $numPuntos) { $cadena.="\n"; } else { $cadena.=",\n"; }
-						echo $cadena;
-						$numPuntos++;
-						$rs->MoveNext();
-					} //while
-		?>
-				]; //Coordsx
-				//Crea la variable del numero del poligo
-				var Pol0<?php echo $numPoly; ?>;
-		  		//Crea el poligo y las propiedades
-				Pol0<?php echo $numPoly; ?> = new google.maps.Polygon({ paths: Coords0<?php echo $numPoly; ?>, strokeColor: '<?php echo $lcolor; ?>', strokeWeight: 0.8, fillColor: '<?php echo $color; ?>', fillOpacity: '<?php echo $fillop; ?>', zIndex: 1 });
-
-		<?php
-				// ************************************************************************************
-				//Arreglo de secciones relacionadas con el numero del poligono
-				$xseccion[$numPoly] = $seccion;
-				$xdistrito[$numPoly] = $ndistrito;
-				$numPoly++;
-				$rs9->MoveNext();
-			} //while*/
+<?php
+		// ************************************************************************************
+		//Arreglo de secciones relacionadas con el numero del poligono
+		$xseccion[$numPoly] = $seccion;
+		$xdistrito[$numPoly] = $ndistrito;
+		$numPoly++;
+		$rs9->MoveNext();
+	} //while*/
 
 
-			$sql1= "SELECT seccion, coord, distrito FROM ".$tbl_seccion." ORDER BY seccion, orden, distrito";
-			$rs = $db->execute($sql1);
-			$ini_seccion = $rs->fields[0];
-			$numPuntos = 1;
-			$numPoly = 1;
-			$xdistrito[$numPoly]=$rs->fields[2];
-			$xseccion[$numPoly]=$ini_seccion;
+	$sql1= "SELECT seccion, coord, distrito FROM ".$tbl_seccion." ORDER BY seccion, orden, distrito";
+	$rs = $db->execute($sql1);
+	$ini_seccion = $rs->fields[0];
+	$numPuntos = 1;
+	$numPoly = 1;
+	$xdistrito[$numPoly]=$rs->fields[2];
+	$xseccion[$numPoly]=$ini_seccion;
 
-			?>//COORDENADAS
-				var sCoords0<?php echo $numPoly; ?> = [
-			<?php
-				while(!$rs->EOF)
-				{
-					$fin_seccion = $rs->fields[0];
-					$ddistrito = $rs->fields[2];
-					if($ini_seccion==$fin_seccion){
-						$cadena = "new google.maps.LatLng(".$rs->fields[1].")";
-						if($numPuntos==0) { $cadena.="\n"; } else { $cadena.=",\n"; }
-						echo $cadena;
-						$numPuntos++;
-					}else{
-						$sql3="SELECT COUNT(*) FROM redistritacion where tipo=1 and seccion='".$ini_seccion."'";
-						$rs3 = $db->execute($sql3);
-						$color="#FFFF00";
-						$lcolor="#666";
-
-						if($rs3->fields[0]>0){
-							$color="#FF0000";
-							$lcolor="#FCD209";
-						}
-			?>
-						];
-						var sPol0<?php echo $numPoly; ?>;
-						sPol0<?php echo $numPoly; ?> = new google.maps.Polygon({ paths: sCoords0<?php echo $numPoly; ?>, strokeColor: '<?php echo $lcolor; ?>', strokeWeight: 0.6, fillColor: '<?php echo $color; ?>', fillOpacity: '<?php echo $fillop; ?>', zIndex: 1});
-			    <?php /****reinicilizo porque cambia el distrito************/
-						$xdistrito[$numPoly]=$ddistrito;
-						$xseccion[$numPoly]=$ini_seccion;
-						$numPoly++;
-						$numPuntos = 1;
-						$ini_seccion = $rs->fields[0];
-				?>
-						var sCoords0<?php echo $numPoly; ?> = [
-				<?php
-						$cadena= "new google.maps.LatLng(".$rs->fields[1].")";
-						$cadena.=",\n";
-						echo $cadena;
-						$numPuntos++;
-					}
-					$rs->MoveNext();
-				} //while
-
-				$color="#FFFF00";
-				$lcolor="#666";
-
+	?>//COORDENADAS
+		var sCoords0<?php echo $numPoly; ?> = [
+	<?php
+		while(!$rs->EOF)
+		{
+			$fin_seccion = $rs->fields[0];
+			$ddistrito = $rs->fields[2];
+			if($ini_seccion==$fin_seccion){
+				$cadena = "new google.maps.LatLng(".$rs->fields[1].")";
+				if($numPuntos==0) { $cadena.="\n"; } else { $cadena.=",\n"; }
+				echo $cadena;
+				$numPuntos++;
+			}else{
 				$sql3="SELECT COUNT(*) FROM redistritacion where tipo=1 and seccion='".$ini_seccion."'";
 				$rs3 = $db->execute($sql3);
+				$color="#FFFF00";
+				$lcolor="#666";
 
 				if($rs3->fields[0]>0){
 					$color="#FF0000";
 					$lcolor="#FCD209";
 				}
-				$xdistrito[$numPoly] = $ddistrito;
-				$xseccion[$numPoly] = $ini_seccion;
-				?>
+	?>
 				];
 				var sPol0<?php echo $numPoly; ?>;
 				sPol0<?php echo $numPoly; ?> = new google.maps.Polygon({ paths: sCoords0<?php echo $numPoly; ?>, strokeColor: '<?php echo $lcolor; ?>', strokeWeight: 0.6, fillColor: '<?php echo $color; ?>', fillOpacity: '<?php echo $fillop; ?>', zIndex: 1});
-		<?php
+	    <?php /****reinicilizo porque cambia el distrito************/
+				$xdistrito[$numPoly]=$ddistrito;
+				$xseccion[$numPoly]=$ini_seccion;
 				$numPoly++;
+				$numPuntos = 1;
+				$ini_seccion = $rs->fields[0];
 		?>
-
-		//-----------------------------------------------------------------------------------------------
-
-			//CENTROIDE DE LA PRIMERA COORDENADA
-			/*var bounds = new google.maps.LatLngBounds();
-			var i;
-			for (i = 0; i < dCoords01.length; i++) {
-		  		bounds.extend(dCoords01[i]);
+				var sCoords0<?php echo $numPoly; ?> = [
+		<?php
+				$cadena= "new google.maps.LatLng(".$rs->fields[1].")";
+				$cadena.=",\n";
+				echo $cadena;
+				$numPuntos++;
 			}
-		  	var myLatLng = bounds.getCenter();*/
-			var myLatLng = new google.maps.LatLng('19.347904','-99.167116');
+			$rs->MoveNext();
+		} //while
 
-			//OPCIONES DE MAPA
-		  	var mapOptions = {
-		    	zoom: 11,
-		    	center: myLatLng,
-				panControl: false,
-				streetViewControl: false,
-				mapTypeControl: true,
-		    	mapTypeControlOptions: {
-		      		style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-		    	},
-				zoomControl: true,
-		    	zoomControlOptions: {
-		      		style: google.maps.ZoomControlStyle.SMALL
-		    	},
-		    	mapTypeId: google.maps.MapTypeId.ROADMAP
-		  	};
+		$color="#FFFF00";
+		$lcolor="#666";
 
-			//Asignar mapa al DIV
-		  	map = new google.maps.Map(document.getElementById('map-canvas2'),
-		      	mapOptions);
-		//-----------------------------------------------------------------------------------------------
+		$sql3="SELECT COUNT(*) FROM redistritacion where tipo=1 and seccion='".$ini_seccion."'";
+		$rs3 = $db->execute($sql3);
 
-		<?php
-		if($lim==1) {
-			for($di=1; $di < $dnumPoly; $di++)
-			{
+		if($rs3->fields[0]>0){
+			$color="#FF0000";
+			$lcolor="#FCD209";
+		}
+		$xdistrito[$numPoly] = $ddistrito;
+		$xseccion[$numPoly] = $ini_seccion;
 		?>
-			  /*dPol0<?php echo $di; ?>.setMap(map);*/ // Comentado por Joel
-		<?php
-			}
-		} //if limites distritales
-		?>
+		];
+		var sPol0<?php echo $numPoly; ?>;
+		sPol0<?php echo $numPoly; ?> = new google.maps.Polygon({ paths: sCoords0<?php echo $numPoly; ?>, strokeColor: '<?php echo $lcolor; ?>', strokeWeight: 0.6, fillColor: '<?php echo $color; ?>', fillOpacity: '<?php echo $fillop; ?>', zIndex: 1});
+<?php
+		$numPoly++;
+?>
 
-		<?php
-			//Asignar las secciones al mapa, crear el escucha del poligono y la funcion a realizar.
-			for($i=1; $i < $numPoly; $i++)
-			{
-		?>
-				sPol0<?php echo $i; ?>.setMap(map);
-				google.maps.event.addListener(sPol0<?php echo $i; ?>, 'click', function(){
-			  		loadXMLDoc4('<?php echo $xseccion[$i]; ?>','<?php echo $xdistrito[$i]; ?>','<?php echo $tipo; ?>','<?php echo "2013"; ?>'); });
-		<?php }  ?>
+//-----------------------------------------------------------------------------------------------
 
-			//Incluir ventada de informacion
-			infoWindow = new google.maps.InfoWindow();
-			//Crear escucha y cerrar ventana de informacion cuando se de un click en el mapa.
-			google.maps.event.addListener(map, 'click', function() { infoWindow.close(); });
-		} //function
+	//CENTROIDE DE LA PRIMERA COORDENADA
+	/*var bounds = new google.maps.LatLngBounds();
+	var i;
+	for (i = 0; i < dCoords01.length; i++) {
+  		bounds.extend(dCoords01[i]);
+	}
+  	var myLatLng = bounds.getCenter();*/
+	var myLatLng = new google.maps.LatLng('19.347904','-99.167116');
 
-			//Cargar mapa cuando inicie la pagina.
-			google.maps.event.addDomListener(window, 'load', initialize);
+	//OPCIONES DE MAPA
+  	var mapOptions = {
+    	zoom: 11,
+    	center: myLatLng,
+		panControl: false,
+		streetViewControl: false,
+		mapTypeControl: true,
+    	mapTypeControlOptions: {
+      		style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    	},
+		zoomControl: true,
+    	zoomControlOptions: {
+      		style: google.maps.ZoomControlStyle.SMALL
+    	},
+    	mapTypeId: google.maps.MapTypeId.ROADMAP
+  	};
+
+	//Asignar mapa al DIV
+  	map = new google.maps.Map(document.getElementById('map-canvas2'),
+      	mapOptions);
+//-----------------------------------------------------------------------------------------------
+
+<?php
+if($lim==1) {
+	for($di=1; $di < $dnumPoly; $di++)
+	{
+?>
+	  dPol0<?php echo $di; ?>.setMap(map);
+<?php
+	}
+} //if limites distritales
+?>
+
+<?php
+	//Asignar las secciones al mapa, crear el escucha del poligono y la funcion a realizar.
+	for($i=1; $i < $numPoly; $i++)
+	{
+?>
+		sPol0<?php echo $i; ?>.setMap(map);
+		google.maps.event.addListener(sPol0<?php echo $i; ?>, 'click', function(){
+	  		loadXMLDoc4('<?php echo $xseccion[$i]; ?>','<?php echo $xdistrito[$i]; ?>','<?php echo $tipo; ?>','<?php echo "2013"; ?>'); });
+<?php } ?>
+
+	//Incluir ventada de informacion
+	infoWindow = new google.maps.InfoWindow();
+	//Crear escucha y cerrar ventana de informacion cuando se de un click en el mapa.
+	google.maps.event.addListener(map, 'click', function() { infoWindow.close(); });
+} //function
+
+	//Cargar mapa cuando inicie la pagina.
+	google.maps.event.addDomListener(window, 'load', initialize);
+
     </script>
 </head>
 <body>
